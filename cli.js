bf047826bde87bf5897ac33566bbc72159a8a531
Chloe Wintzer <chloe.wintzer@rearc.io>
2018-07-24 17:03:32 -0400

    Report version at start

--- cli.js
+++ cli.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const cli = require('yargs')
 const debug = (process.env.DEBUG === 'true')
+const version = require('./package.json').version
 
 const runCommand = require('./lib/commands/run')
+
+console.log(`Noop CLI v${version}`)
 
 cli.command('run [port]', 'run local dev server', (yargs) => {
   yargs.positional('port', {
