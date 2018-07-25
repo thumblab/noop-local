2ac8f8b44d1a9b51cb694306eae3e6d863884288
Chloe Wintzer <chloe.wintzer@rearc.io>
2018-07-25 19:32:03 -0400

    Adds `noop inspect` command

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ const debug = (process.env.DEBUG === 'true')
 const version = require('./package.json').version
 
 const runCommand = require('./lib/commands/run')
+const inspectCommand = require('./lib/commands/inspect')
 
 console.log(`Noop CLI v${version}`)
 
@@ -12,9 +13,9 @@ cli.command('run [port]', 'run local dev server', (yargs) => {
     describe: 'port to bind local dev server',
     default: 4444
   })
-}, runCommand((err) => {
-  console.log('FINAL', err)
-}))
+}, runCommand)
+
+cli.command('inspect', 'inspect noop app', inspectCommand)
 
 const argv = cli.argv // no idea wtf reading `argv` props does to make it work
 if (debug) console.log(argv)
