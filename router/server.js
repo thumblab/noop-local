ba29f07ff0c96b35afa17f4ae0ad3ab6fc2deddd
Chloe Wintzer <chloe.wintzer@rearc.io>
2018-09-24 15:51:21 -0400

    Additional CORS support

--- router/server.js
+++ router/server.js
@@ -9,6 +9,7 @@ let requestCount = 0
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
   next()
 })
 
