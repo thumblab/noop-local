f25d8cd142e344762495c44664e01632892b23f5
Chloe Wintzer <chloe.wintzer@rearc.io>
2019-04-02 16:52:56 -0700

    scaffolding for inspector console

---
+++ lib/inspector/console/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <b-container fluid id="main">
+      <ErrorMessages />
+      <router-view/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+  background-color: #f6f8fa !important
+}
+</style>
---
+++ lib/inspector/console/src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    noop local inspector
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
